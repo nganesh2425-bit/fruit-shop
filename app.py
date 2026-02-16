d7a7b94ca5a83e01dabd37203644bdad19d17be0
Ganesh Nowdasari <nganesh2425@gmail.com>
2026-02-16 17:29:02 +0530

    changes has done

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ fruits = [
     {"name": "Apple", "price": 120, "image": "https://cdn-icons-png.flaticon.com/512/415/415733.png"},
     {"name": "Banana", "price": 40, "image": "https://cdn-icons-png.flaticon.com/512/590/590685.png"},
     {"name": "Orange", "price": 80, "image": "https://cdn-icons-png.flaticon.com/512/415/415682.png"},
-    {"name": "Mango", "price": 150, "image": "https://cdn-icons-png.flaticon.com/512/590/590696.png"},
-    {"name": "Grapes", "price": 90, "image": "https://cdn-icons-png.flaticon.com/512/590/590681.png"},
     {"name": "Watermelon", "price": 200, "image": "https://cdn-icons-png.flaticon.com/512/590/590694.png"},
 ]
 
